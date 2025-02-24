cf88ba898221007ec26a66c8d7c261f5f2da12fb
unknown <stefanogabriela1@gmail.com>
2025-02-24 19:27:40 +0200

    feature/homepage

--- public/projects/js/projects.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.view-details').forEach(button => {
-    button.addEventListener('click', (e) => {
-        e.stopPropagation();
-        const projectTitle = e.target.parentElement.querySelector('.project-title').textContent;
-        alert(`Проект: ${projectTitle}\nСкоро ще добавим повече детайли!`);
-    });
-});
